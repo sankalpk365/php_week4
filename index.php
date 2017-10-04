e62227de176a331e4d7dd4dcaef891a955720473
sankalp arun karkera <sk2545@afsconnect1.njit.edu>
2017-10-04 12:21:37 -0400

    first commit

--- index.php
+++ index.php
@@ -6,7 +6,17 @@ $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
-echo "The value of \$year: ";
-print_r($year)
+echo "The value of \$year: "."<br>";
+print_r($year)."<br>";
+echo "<br>";
 
-?>
+echo "<br>1. Replaced '-' with '/' in date <br>";
+$date1 = str_replace("-", "/","$date");
+echo "The value of \$date: ".$date1."<br>";
+echo "<br>";
+
+
+
+
+
+>
